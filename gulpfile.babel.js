00000b971bc590df9c92a5d867ee098327a8d155
Brian Blosser <bpb54321@gmail.com>
2017-08-15 17:06:33 -0400

    Created the gulpfile that should allow compiling ES6 to ES5, and using import on modules.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,16 @@
 import gulp from 'gulp';
+import babelify from 'babelify';
+import browserify from 'browserify';
+import buffer from 'vinyl-buffer';
+import source from 'vinyl-source-stream';
 
 gulp.task('default', () => {
-  console.log('Default task called.');
+  var bundler = browserify('app.js.es6');
+  bundler.transform(babelify);
+
+  bundler.bundle()
+    .on('error', (err) => { console.error(err); })
+    .pipe(source('src/app.js'))
+    .pipe(buffer())
+    .pipe(gulp.dest('dest'));
 });
