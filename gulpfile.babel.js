c4f1cc217bda98beb412ebcd9a0f976e81fa78e3
Brian Blosser <bpb54321@gmail.com>
2017-08-16 13:50:35 -0400

    Changed the gulpfile so that it transpiles app.js and bundles all it's modules into bundle.js.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,12 +5,12 @@ import buffer from 'vinyl-buffer';
 import source from 'vinyl-source-stream';
 
 gulp.task('default', () => {
-  var bundler = browserify('app.js.es6');
+  var bundler = browserify('app.js');
   bundler.transform(babelify);
 
   bundler.bundle()
     .on('error', (err) => { console.error(err); })
-    .pipe(source('src/app.js'))
+    .pipe(source('bundle.js'))
     .pipe(buffer())
-    .pipe(gulp.dest('dest'));
+    .pipe(gulp.dest('./'));
 });
